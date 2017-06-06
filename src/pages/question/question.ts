acd6794b387809492d87e537b549fe655ed43351
Gerardo Soto <gerardosoto93@gmail.com>
2017-06-06 07:13:13 +0000

    Resolved Question.ts merge conflict

--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -3,12 +3,6 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 import { QuestionsProvider } from '../../providers/questions/questions';
 
 
-/**
- * Generated class for the QuestionPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 
 
 @IonicPage()
