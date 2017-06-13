5ca21cb707693185d8d783a494a0fa849938fc51
Gerardo Soto <gerardosoto93@gmail.com>
2017-06-13 02:07:18 +0000

    Saved test results

--- src/providers/test-results/test-results.ts
+++ src/providers/test-results/test-results.ts
@@ -12,7 +12,7 @@ import 'rxjs/add/operator/map';
 export class TestResultsProvider {
 
   //Strongloop API Explorer's request URL
-  baseUrl: string = "https://sp-17-jenny-jbrownssf.c9users.io:8080/api";
+  baseUrl: string = "https://sp-17-salvatore-jbrownssf.c9users.io:8080/api";
   path: string = "/TestResults";
 
 
@@ -23,17 +23,31 @@ export class TestResultsProvider {
 
   //Retrieve test results with access token
   getTestResults(token){
-    return this.http.get(this.baseUrl + this.path + '?access_token=' + token);
+    return this.http.get(
+      this.baseUrl + 
+      this.path + 
+      '?access_token=' + token
+    );
   }
 
   //Save a new test result to the backend
   saveTest(token, testResult){
-    return this.http.post(this.baseUrl + this.path + '?access_token=' + token, testResult);
+    return this.http.post(
+      this.baseUrl + 
+      this.path + 
+      '?access_token=' + token, 
+      testResult
+    );
   }
 
   //Delete a test result according to id
   deleteTest(testId){
-    return this.http.delete(this.baseUrl + this.path + '/' + testId );
+    return this.http.delete(
+      this.baseUrl + 
+      this.path + 
+      '/' + 
+      testId
+    );
   }
 
 
