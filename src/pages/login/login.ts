2aed4bdc11d109b7cf46b43e86471107d6021b55
SalMac86 <salvatore.mccarty@gmail.com>
2017-06-13 03:53:17 +0000

    setting up login and register

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
 import { LobbyPage } from '../lobby/lobby';
+
+import { AppUsersProvider } from '../../providers/app-users/app-users';
 /**
  * Generated class for the LoginPage page.
  *
@@ -13,15 +16,52 @@ import { LobbyPage } from '../lobby/lobby';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user: any = {};
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private appUsers: AppUsersProvider
+    ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
-  logMeIn() {
-    this.navCtrl.push(LobbyPage);
+  logForm(form){
+    console.log(form);
+    if(form.invalid){
+      return alert("Please fill in all required fields!");
+    }
+    this.appUsers.login(this.user)
+      .map(res => res.json())
+      .subscribe(res => {
+        window.localStorage.setItem('token', res.id);
+        window.localStorage.setItem('userId', res.userId);
+        this.navCtrl.setRoot(LobbyPage);
+        //handle successful responses
+        // console.log("hope this worked");
+      }, 
+      error => {
+      // switch(error.status) {
+        // case 404:
+        //   alert("404: Page Not Found");
+        //   break;
+        // case 422:
+        //   alert("That e-mail is already in use.");
+        //   break;
+        // case null:
+        //   alert("User is offline.");
+        //   break;
+        // case 500:
+        //   alert("THE SKY IS FALLING!!!");
+        //   break;
+        // default:
+          console.log("nope, didn't work cuz: "+error.status);
+          // break;
+      })
+
+    //do stuff
+    
   }
 }
